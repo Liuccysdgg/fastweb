3887b5f569848c117c2fe3288404ce4923ca40c1
xx <a1585346868@hotmail.com>
2024-05-28 15:47:26 +0800

    增加编译脚本

---
+++ build.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+# 创建3rdparty目录
+mkdir -p 3rdparty
+
+# 进入3rdparty目录
+cd 3rdparty
+
+# 检查ylib是否已经克隆
+if [ ! -d "ylib" ]; then
+    # 克隆ylib C++开发库
+    git clone https://github.com/Liuccysdgg/ylib.git
+
+   
+else
+    echo "ylib exist"
+fi
+ # 进入ylib目录
+cd ylib
+chmod 777 build.sh
+# 调用buildl.sh进行编译
+./build.sh
+
+# 返回3rdparty目录
+cd ..
+# 返回最初的目录
+cd ..
+
+# 克隆fastweb仓库
+if [ ! -d "fastweb" ]; then
+    git clone https://github.com/Liuccysdgg/fastweb.git
+fi
+
+# 进入fastweb目录
+cd fastweb
+
+# 创建build目录
+mkdir -p build
+
+# 进入build目录
+cd build
+
+# 使用cmake配置fastweb代码
+cmake ..
+
+# 编译fastweb
+make
+make install
