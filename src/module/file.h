f74c26c54caa69a8e32fc7630b0bc1602ed2c25b
xx <a1585346868@hotmail.com>
2024-06-03 23:39:22 +0800

    增加功能

--- src/module/file.h
+++ src/module/file.h
@@ -12,6 +12,16 @@ namespace module
 		static sol::table list(const std::string& dirpath,const std::string& regex,sol::this_state s);
 		static void copy_dir(const std::string& src_dir,const std::string& dst_dir);
 		static bool create_dir(const std::string& dirpath);
+		static sol::object read(const std::string& filepath,sol::this_state s);
+		static bool write(const std::string& filepath,const std::string& data);
+		static bool remove(const std::string& filepath);
+		static bool remove_dir(const std::string& dirpath, bool recycle);
+		static bool exist(const std::string& filepath);
+		static bool exist_dir(const std::string& dirpath);
+		static int64 size(const std::string& filepath);
+		static bool copy(const std::string& src, const std::string& dst);
+		static timestamp last_write_time(const std::string& filepath);
+
 		static void regist(sol::state* lua);
 	};
 
--- src/module/sys.h
+++ src/module/sys.h
@@ -10,6 +10,14 @@ namespace module
 	class sys:public module::imodule {
 	public:
 		static std::string current_dir();
+		static void sleep_msec(uint32 msec);
+		static int64 random(int64 min, int64 max);
+		static std::string current_filepath();
+		static std::string temp_path();
+		static std::string desktop_path();
+		static std::string currentuser_path();
+		static sol::table mac(sol::this_state s);
+
 		static void regist(sol::state* lua);
 	};
 
