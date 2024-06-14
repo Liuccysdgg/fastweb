cc7f97c189bdff6ba18cd3b91720de085d8b2edd
xx <a1585346868@hotmail.com>
2024-06-14 23:10:26 +0800

    更新许可

--- src/module/http/subscribe.cpp
+++ src/module/http/subscribe.cpp
@@ -1,28 +1,19 @@
-﻿// MIT License﻿
-// Copyright(c) 2024 FastWeb - fwlua.com - nianhua﻿
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files(the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and /or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions :﻿
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.﻿
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-// SOFTWARE.﻿
-// ## Additional Terms for Commercial Use﻿
-// This software is licensed for personal, educational, and non - commercial use.
-// For commercial use or use within a company, organization, or institution, a
-// separate commercial license is required.To obtain a commercial license,
-// please contact
-// EMail：1585346868@qq.com
+﻿/*Software License
 
-// QQ：1585346868
+Copyright(C) 2024[liuyingjie]
+License Terms
+Usage Rights
+
+Any individual or entity is free to use, copy, and distribute the binary form of this software without modification to the source code, without the need to disclose the source code.
+If the source code is modified, the modifications must be open - sourced under the same license.This means that the modifications must be disclosed and accompanied by a copy of this license.
+Future Versions Updates
+From this version onwards, all future releases will be governed by the terms of the latest version of the license.This license will automatically be nullified and replaced by the new version.
+Users must comply with the terms of the new license issued in future releases.
+Liability and Disclaimer
+This software is provided “as is”, without any express or implied warranties, including but not limited to the warranties of merchantability, fitness for a particular purpose, and non - infringement.In no event shall the author or copyright holder be liable for any claims, damages, or other liabilities, whether in an action of contract, tort, or otherwise, arising from, out of, or in connection with the software or the use or other dealings in the software.
+Contact Information
+If you have any questions, please contact us: 1585346868@qq.com Or visit our website fwlua.com.
+*/
 
 #include "subscribe.h"
 #include "core/app.h"
